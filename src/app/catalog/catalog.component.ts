69a40cc50ad80d141340ba2813ca15a52402aff1
Rene Copaga <rene.copaga@gmail.com>
2019-03-08 08:42:05 -0400

    Details

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,6 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from '../shared/Item';
 
+const ITEMS: Item[] = [
+  {
+    name: 'Helmet',
+    image: '/assets/images/helmet.jpg',
+    category: 'safety',
+    label: 'ultra safe',
+    price: '4.99',
+    description: 'Un casco de seguridad útil para proteger en ámbitos como la construcción, fabricas y otros'
+  },
+  {
+    name: 'Audio protector',
+    image: '/assets/images/audio.jpg',
+    category: 'safety',
+    label: 'noise filter',
+    price: '12.99',
+    description: 'Protege de ruidos auditivos por encima de los decibeles considerados aceptables'
+  },
+  {
+    name: 'Glasses',
+    image: '/assets/images/glasses.jpg',
+    category: 'safety',
+    label: 'New technology',
+    price: '31.99',
+    description: 'Permite filtrar rayos ultravioleta, material anti rayas, anti caidas'
+  },
+  {
+    name: 'Gloves',
+    image: '/assets/images/gloves.jpg',
+    category: 'safety',
+    label: '',
+    price: '12.99',
+    description: 'Hechos de algodón con goma antideslizante, especial para industria química.'
+  }
+];
+
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
@@ -8,41 +43,8 @@ import {Item} from '../shared/Item';
 })
 export class CatalogComponent implements OnInit {
 
-  // @ts-ignore
-  items: Item[] = [
-    {
-      name: 'Helmet',
-      image: '/assets/images/helmet.jpg',
-      category: 'safety',
-      label: 'ultra safe',
-      price: '4.99',
-      description: 'Un casco de seguridad útil para proteger en ámbitos como la construcción, fabricas y otros'
-    },
-    {
-      name: 'Audio protector',
-      image: '/assets/images/audio.jpg',
-      category: 'safety',
-      label: 'noise filter',
-      price: '12.99',
-      description: 'Protege de ruidos auditivos por encima de los decibeles considerados aceptables'
-    },
-    {
-      name: 'Glasses',
-      image: '/assets/images/glasses.jpg',
-      category: 'safety',
-      label: 'New technology',
-      price: '31.99',
-      description: 'Permite filtrar rayos ultravioleta, material anti rayas, anti caidas'
-    },
-    {
-      name: 'Gloves',
-      image: '/assets/images/gloves.jpg',
-      category: 'safety',
-      label: '',
-      price: '12.99',
-      description: 'Hechos de algodón con goma antideslizante, especial para industria química.'
-    }
-  ];
+  items = ITEMS;
+  selectedItem = ITEMS[0];
 
   constructor() { }
 
